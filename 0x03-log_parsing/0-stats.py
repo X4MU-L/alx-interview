6893c3d98c25135ecad2f0cdcdb936ec5b9f2e35
Chukwuebuka Okoli <okolisamuel21@gmail.com>
2023-10-20 01:06:24 +0100

    (feat) -> create log passing function

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""
+    this script reads from stdin line by line and computes metrics
+"""
+
+
+import sys
+import signal
+
+
+total_size = 0
+result = {}
+
+
+def display_metrics():
+    """displays the metrics information in a format"""
+    print("File size: {}".format(total_size))
+    for key in sorted(result):
+        print(f"{key}: {result[key]}")
+
+
+def sigint_handler(signum, frame):
+    """handles a sigint signal"""
+    display_metrics()
+    exit(1)
+
+
+signal.signal(signal.SIGINT, sigint_handler)
+valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+count = 0
+
+
+for line in sys.stdin:
+    if count == 10:
+        count = 1
+        display_metrics()
+    else:
+        count += 1
+
+    try:
+        line = line.split()
+        status_code, file_size = line[-2], line[-1]
+        if status_code in valid_codes:
+            try:
+                total_size += int(file_size)
+                if status_code not in result:
+                    result[status_code] = 1
+                else:
+                    result[status_code] += 1
+            except Exception as e:
+                continue
+    except IndexError:
+        continue
